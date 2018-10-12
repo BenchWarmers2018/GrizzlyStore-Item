4a878535b3d5ddf3f7ab3279e8747655411b262d
a-nthonymai <Anthony.Mai@hotmail.com>
2018-10-12 16:04:14 +1100

    Added edit categories functionality

--- src/main/java/com/benchwarmers/grads/grizzlystoreitem/repositories/CategoryRepository.java
+++ src/main/java/com/benchwarmers/grads/grizzlystoreitem/repositories/CategoryRepository.java
@@ -9,16 +9,11 @@ import java.util.List;
 
 @Repository
 public interface CategoryRepository extends JpaRepository<Category, Integer> {
-//    @Query(value = "SELECT category_name FROM category", nativeQuery = true)
-//    List<Category> findAllCategories();
-    //List<Category> find
     Category findCategoryByIdCategory(Integer id);
     boolean existsByIdCategory(Integer id);
     Category findCategoryByCategoryName(String name);
     Page findCategoryByCategoryName(String name, Pageable pageable);
     boolean existsByCategoryName(String name);
-
     Category findCategoriesByItemsContaining(Integer itemId);
     boolean existsCategoryByItemsContaining(Integer itemId);
-
 }
