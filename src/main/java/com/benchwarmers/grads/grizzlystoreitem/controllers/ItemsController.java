064f2f25beab5bb2631548f20e1a998efbab8636
Singh <723304@192-168-1-5.tpgi.com.au>
2018-10-16 22:38:08 +1100

    Home page method

--- src/main/java/com/benchwarmers/grads/grizzlystoreitem/controllers/ItemsController.java
+++ src/main/java/com/benchwarmers/grads/grizzlystoreitem/controllers/ItemsController.java
@@ -65,7 +65,7 @@ public class ItemsController
 
 
     /* localhost:8080/items/page?page=0&size=5 */
-
+    @RequestMapping(path = "/page")
     public Page<Item> getPagedItems (@RequestParam Integer size, @RequestParam Integer page) {
         Page<Item> p = itemRepository.findAll(PageRequest.of(page, size));
         return p;
